58589714528e3fca7096443cdd0594dbd8836fd0
John-C <catchings_john@bah.com>
2020-04-17 14:35:42 -0600

    removed most global variables

--- Euler #96.py
+++ Euler #96.py
@@ -1,5 +1,6 @@
 from timeit import default_timer
 
+
 def read_sudoku(grid_number):
     if grid_number < 10:
         grid_number = '0'+str(grid_number)
@@ -14,36 +15,34 @@ def read_sudoku(grid_number):
     return sudoku
 
 
-def get_possible(y, x):
-    global sudoku
+def get_possible(y, x, puzzle):
     possible = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-    column = sudoku[y]
-    row = [position[x] for position in sudoku]
+    column = puzzle[y]
+    row = [position[x] for position in puzzle]
     grid_y, grid_x = 3*(y//3), 3*(x//3)
     grid = set()
     for i in range(3):
         for j in range(3):
-            grid.add(sudoku[grid_y+i][grid_x+j])
+            grid.add(puzzle[grid_y+i][grid_x+j])
 
     return possible - set(column) - set(row) - set(grid)
 
 
-def solve():
+def solve(puzzle):
 
-    global sudoku
     global complete
     for y in range(9):
         for x in range(9):
-            if sudoku[y][x] == 0:
-                p = get_possible(y, x)
+            if puzzle[y][x] == 0:
+                p = get_possible(y, x, puzzle)
                 for num in p:
-                    sudoku[y][x] = num
-                    solve()
-                    sudoku[y][x] = 0
+                    puzzle[y][x] = num
+                    solve(puzzle)
+                    puzzle[y][x] = 0
 
-                return
+                return complete
 
-    complete = [line[:] for line in sudoku]
+    complete = [line[:] for line in puzzle]
 
     return
 
@@ -51,9 +50,9 @@ def solve():
 start = default_timer()
 nums = []
 for i in range(1, 51):
-    complete = None
     sudoku = read_sudoku(i)
-    solve()
+    complete = None
+    solve(sudoku)
     nums.append(int(''.join((str(i) for i in complete[0][:3]))))
 
 stop = default_timer()
